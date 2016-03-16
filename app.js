1520b7ea09fa2c9e57b2691539789760a52fb187
onwezen <joshua.onwezen@hva.nl>
2016-03-16 19:42:39 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+
+var Promise = require("bluebird");
+var fs = Promise.promisifyAll(require("fs"));
+
+Promise.try(function(){
+	return fs.readFileAsync("./config.json").then(JSON.parse);
+}).catch({code: "ENOENT"}, function(err){
+	/* Return an empty object. */
+	return Promise.resolve({});
+}).then(function(config){
+	/* `config` now either contains the JSON-parsed configuration file, or an empty object if no configuration file existed. */
+});
+
+//https://gist.github.com/joepie91/f6a56acdae303e90e44a
